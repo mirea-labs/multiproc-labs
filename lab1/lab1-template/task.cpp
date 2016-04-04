8effcffeb39d702190ac99df12f0740d1ecb6ae2
sinbad228228 <sonic963852741@mail.ru>
2016-04-04 12:17:45 +0300

    Lab 1 Sadekov

--- lab1/lab1-template/task.cpp
+++ lab1/lab1-template/task.cpp
@@ -1,8 +1,45 @@
 
 #include "stdafx.h"
+#include <iostream>
+#include "stdlib.h"
+#include <string>
+#include "conio.h"
+#include <stdio.h>
+#include "omp.h"
+
+
+using namespace std;
 
 int performArrayTask(int size)
 {
 	//TODO: add code here
+	long *arr = new long[size];
+	long i;
+	long sum = 0; // сумма элементов массива
+	double start_time, end_time;
+
+	for (i=0;i<size;i++){
+		arr[i] = i;
+	};
+
+	start_time = omp_get_wtime();
+#pragma omp parallel num_threads(4)
+	{
+		long local_sum = 0;
+#pragma omp for
+	for (i=0; i<size; i++){  
+		sum += arr[i];
+	};
+
+	sum+=local_sum;
+
+	end_time = omp_get_wtime(); 
+
+	}
+
+	cout <<"Summa: " <<sum << endl;
+    printf("Zamer vremeni %lf\n", end_time-start_time); 
+
+	delete []arr;
 	return 0;
 }
