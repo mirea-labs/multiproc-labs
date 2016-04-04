d1d6087b063706072c1e70a66d0ae1449b04b016
Alexsander1994 <SanyaSerp@bk.ru>
2016-04-04 12:02:00 +0300

    lab_rab1

--- lab1/lab1-template/task.cpp
+++ lab1/lab1-template/task.cpp
@@ -1,8 +1,38 @@
 
 #include "stdafx.h"
+#include "iostream"
+#include <stdio.h>
+
+
+using namespace std;
 
 int performArrayTask(int size)
 {
 	//TODO: add code here
+	setlocale(LC_ALL, "Russian");
+
+	long *arr = new long[size];
+	long sum=0;
+	double start_time, end_time;
+	int i;
+	
+	for (long i=0; i< size; i++)
+	{
+		arr[i]=i;
+	}
+	start_time = omp_get_wtime();
+#pragma omp parallel num_threads(8)
+	{
+#pragma omp for 
+		for (long i=0; i< size; i++)
+		{
+			sum +=arr[i];
+		}
+	}
+	end_time = omp_get_wtime(); 
+	cout << sum << endl;
+	printf("замер времени %lf\n", end_time-start_time); 
+	system("pause");
+
 	return 0;
 }
